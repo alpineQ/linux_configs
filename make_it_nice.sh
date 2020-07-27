45c0693ea405965ba0b55698c8a64772356b2ceb
justaccount <justaccount@1.ru>
2020-07-27 10:31:24 +0300

    Проверка уже настроенных ssh-ключей

--- make_it_nice.sh
+++ make_it_nice.sh
@@ -1,7 +1,10 @@
 cp .bashrc ~
 sudo apt install -y build-essential cmake vim python3-dev git tmux docker docker-compose nodejs npm xclip
 
-ssh-keygen -b 2048 -t rsa -q -N ""
+if [ ! -d "$HOME/.ssh"]; then
+    ssh-keygen -b 2048 -t rsa -q -N ""
+fi
+
 git config --global user.email "mk_dev@mail.ru"
 git config --global user.username "alpineQ"
 
@@ -10,7 +13,7 @@ sudo usermod -aG docker $USER
 echo Setting up vim stuff
 cp .vimrc ~
 git clone https://github.com/VundleVim/Vundle.vim.git .vim/bundle/Vundle.vim
-npm install -g livedown
+sudo npm install -g livedown
 echo "Make :PluginInstall" > vim.tmp
 vim vim.tmp
 rm vim.tmp
