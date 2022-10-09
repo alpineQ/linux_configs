bd87b73aa2196acb59c8e29f27dda7e8a353231d
alpineQ <mk_dev@mail.ru>
2022-10-09 12:42:53 +0300

    Installation instruction for vscode and alacritty

--- install.sh
+++ install.sh
@@ -200,9 +200,25 @@ git_login() {
     git config --global user.name "$GIT_USERNAME"
 }
 
+install_vscode() {
+    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+    sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+    sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+    sudo apt install apt-transport-https
+    sudo apt update
+    sudo apt install code
+}
+
+install_alacritty() {
+    sudo add-apt-repository ppa:aslatter/ppa
+    sudo apt install alacritty
+}
+
 install_desktop_tools() {
     echo "INSTALL LOG: INSTALLING DESKTOP TOOLS"
     install_packages gparted telegram-desktop vlc
+    install_vscode
+    install_alacritty
 }
 
 repo_origin() {
